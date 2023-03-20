5bb1e6fffa5d86b172c441e89181ef4579c422e4
Ali Qureshi <parorrey@yahoo.com>
2023-03-20 07:39:46 +0500

    Task 2(e). completed. Added a column named "Attached Objects" in the Media Library Table that displays a comma-separated list of IDs linked to the corresponding edit page.

--- functions.php
+++ functions.php
@@ -16,80 +16,169 @@ if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
  *
  * @return bool      True if metabox should show
  */
-function pscm_show_if_front_page( $cmb ) {
-	// Don't show this metabox if it's not the front page template.
-	if ( get_option( 'page_on_front' ) !== $cmb->object_id ) {
-		return false;
-	}
-	return true;
+
+
+function pscm_get_image_count( $id ){
+    global $wpdb;
+
+    $att  = get_post_custom( $id );
+    $file = $att['_wp_attached_file'][0];
+    //Dot take full path as different image sizes could
+    // have different month, year folders due to theme and image size changes
+    $file = sprintf( "%s.%s",
+        pathinfo( $file, PATHINFO_FILENAME ),
+        pathinfo( $file, PATHINFO_EXTENSION )
+    );
+
+    $sql = "SELECT {$wpdb->posts}.ID 
+        FROM {$wpdb->posts} 
+        INNER JOIN {$wpdb->postmeta} 
+        ON ({$wpdb->posts}.ID = {$wpdb->postmeta}.post_id) 
+        WHERE {$wpdb->posts}.post_type IN ('post', 'page', 'event') 
+        AND (({$wpdb->posts}.post_status = 'publish')) 
+        AND ( ({$wpdb->postmeta}.meta_key = '_thumbnail_id' 
+            AND CAST({$wpdb->postmeta}.meta_value AS CHAR) = '%d') 
+            OR ( {$wpdb->posts}.post_content LIKE %s )
+        ) 
+        GROUP BY {$wpdb->posts}.ID";
+
+    $prepared_sql = $wpdb->prepare( $sql, $id, "%src=\"%".$wpdb->esc_like( $file )."\"%" );
+
+    $post_ids  = $wpdb->get_results( $prepared_sql );
+
+    $count = count( $post_ids );
+
+    return $count;
+}
+function pscm_not_allowed_to_delete($id){
+    pscm_get_image_count($id);
+} 
+// function pscm_remove_media_delete_link_in_grid_view( $response, $attachment ) {
+//     if ( pscm_not_allowed_to_delete( $attachment->ID ) )
+//         $response['nonces']['delete'] = false;
+
+//     return $response;
+// }
+// add_filter( 'wp_prepare_attachment_for_js', 'pscm_remove_media_delete_link_in_grid_view' );
+
+
+add_filter( 'manage_media_columns', 'pscm_attached_objects_col', 10, 2);
+ function pscm_attached_objects_col( $cols, $detached ) {
+	$cols['attached_objects'] =  __( 'Attached Objects', 'pim-safe-category-media' );
+
+	return $cols;
+}
+
+add_action( 'manage_media_custom_column', 'pscm_get_data_attached_objects' , 10, 2 );
+
+function pscm_get_data_attached_objects( $col, $id ) {
+	if($col == 'attached_objects'){
+	
+			$linked_objects = pscm_get_linked_objects( $id );
+			$posts = $linked_objects['posts'];
+			$terms = $linked_objects['terms'];
+
+			foreach ( $posts as $post ){
+				edit_post_link($post->ID, '<strong>', '</strong>, ', $post->ID);
+            
+			}
+
+			foreach ( $terms as $term ){
+				 edit_term_link($term->term_id, '<strong>', '</strong>, ', $term);
+            
+			}
+		}
 }
 
 /**
- * Conditionally displays a field when used as a callback in the 'show_on_cb' field parameter
- *
- * @param  CMB2_Field $field Field object.
- *
- * @return bool              True if metabox should show
+ * Returns an array of of posts and terms arrays with IDs and post titles
+ * of posts where the media library image is in use
+ * as featured image or in the body of post or as term image
+ * @param int $id
+ * @return array
  */
-function pscm_hide_if_no_cats( $field ) {
-	// Don't show this field if not in the cats category.
-	if ( ! has_tag( 'cats', $field->object_id ) ) {
-		return false;
-	}
-	return true;
+function pscm_get_linked_objects( $id ){
+	global $wpdb;
+
+	$att  = get_post_custom( $id );
+	$file = $att['_wp_attached_file'][0];
+	// Do not take full path as different image sizes could
+	// have different month, year folders due to theme and image size changes
+	$name = pathinfo( $file, PATHINFO_FILENAME );
+	$ext  = pathinfo( $file, PATHINFO_EXTENSION );
+
+	$sql = <<<SQL
+select distinct {$wpdb->posts}.ID, {$wpdb->posts}.post_type
+from {$wpdb->posts}
+inner join {$wpdb->postmeta}
+on {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id
+where
+	post_type not in ( 'attachment', 'revision', 'nav_menu_item' )
+	/*and ( {$wpdb->posts}.post_status = 'publish' )*/
+	and (
+		(
+			{$wpdb->postmeta}.meta_key = '_thumbnail_id'
+			and cast({$wpdb->postmeta}.meta_value as char) = '%d'
+		)
+		or ( {$wpdb->posts}.post_content like %s )
+		/*or ( {$wpdb->term_meta}.meta_key ='pscm_term_avatar_id' )*/
+	)
+group by {$wpdb->posts}.ID
+SQL;
+
+ $response['posts'] = $wpdb->get_results( $wpdb->prepare(
+		$sql,
+		$id,
+			"%src=\"%"
+			. like_escape( $name )
+			. "%"
+			. like_escape( $ext )
+			. "\"%"
+	) );
+
+	$response['terms'] = pscm_get_category_terms($id);
+	
+	return $response;
+
 }
 
 /**
- * Manually render a field.
- *
- * @param  array      $field_args Array of field arguments.
- * @param  CMB2_Field $field      The field object.
+ * Returns an array of terms with IDs 
+ * where the media library image is in use
+ * as term image
+ * @param int $id
+ * @return array
  */
-function pscm_render_row_cb( $field_args, $field ) {
-	$classes     = $field->row_classes();
-	$id          = $field->args( 'id' );
-	$label       = $field->args( 'name' );
-	$name        = $field->args( '_name' );
-	$value       = $field->escaped_value();
-	$description = $field->args( 'description' );
-	?>
-	<div class="custom-field-row <?php echo esc_attr( $classes ); ?>">
-		<p><label for="<?php echo esc_attr( $id ); ?>"><?php echo esc_html( $label ); ?></label></p>
-		<p><input id="<?php echo esc_attr( $id ); ?>" type="text" name="<?php echo esc_attr( $name ); ?>" value="<?php echo $value; ?>"/></p>
-		<p class="description"><?php echo esc_html( $description ); ?></p>
-	</div>
-	<?php
+function pscm_get_category_terms($id){
+
+	$args = array(
+        'taxonomy' => 'category',
+        'meta_query' => array(
+             array(
+                'key'       => 'pscm_term_avatar_id',
+                'value'     => $id,
+                'compare'   => '='
+             )
+        )
+    );
+
+	$terms = get_terms( $args );
+ 
+	return $terms;
 }
 
+add_action( 'admin_print_styles-upload.php', 'pscm_column_attached_objects' );
 /**
- * Manually render a field column display.
- *
- * @param  array      $field_args Array of field arguments.
- * @param  CMB2_Field $field      The field object.
+ * Adjust Attached Objects column on Media Library page in WP admin
  */
-function pscm_display_text_small_column( $field_args, $field ) {
-	?>
-	<div class="custom-column-display <?php echo esc_attr( $field->row_classes() ); ?>">
-		<p><?php echo $field->escaped_value(); ?></p>
-		<p class="description"><?php echo esc_html( $field->args( 'description' ) ); ?></p>
-	</div>
-	<?php
+function pscm_column_attached_objects() {
+	echo
+	'<style>
+		.fixed .column-attached_objects {
+			width: 10%;
+		}
+	</style>';
 }
-
-/**
- * Conditionally displays a message if the $post_id is 2
- *
- * @param  array      $field_args Array of field parameters.
- * @param  CMB2_Field $field      Field object.
- */
-function pscm_before_row_if_2( $field_args, $field ) {
-	if ( 2 == $field->object_id ) {
-		echo '<p>Testing <b>"before_row"</b> parameter (on $post_id 2)</p>';
-	} else {
-		echo '<p>Testing <b>"before_row"</b> parameter (<b>NOT</b> on $post_id 2)</p>';
-	}
-}
-
 
 add_action( 'cmb2_admin_init', 'pscm_register_taxonomy_metabox' );
 /**
@@ -123,140 +212,4 @@ function pscm_register_taxonomy_metabox() {
 	) );
 
 
-}
-
-
-add_action( 'cmb2_admin_init', 'pscm_register_theme_options_metabox' );
-/**
- * Hook in and register a metabox to handle a theme options page and adds a menu item.
- */
-function pscm_register_theme_options_metabox() {
-
-	/**
-	 * Registers options page menu item and form.
-	 */
-	$cmb_options = new_cmb2_box( array(
-		'id'           => 'pscm_theme_options_page',
-		'title'        => esc_html__( 'Theme Options', 'cmb2' ),
-		'object_types' => array( 'options-page' ),
-
-		/*
-		 * The following parameters are specific to the options-page box
-		 * Several of these parameters are passed along to add_menu_page()/add_submenu_page().
-		 */
-
-		'option_key'      => 'pscm_theme_options', // The option key and admin menu page slug.
-		'icon_url'        => 'dashicons-palmtree', // Menu icon. Only applicable if 'parent_slug' is left empty.
-		// 'menu_title'              => esc_html__( 'Options', 'cmb2' ), // Falls back to 'title' (above).
-		// 'parent_slug'             => 'themes.php', // Make options page a submenu item of the themes menu.
-		// 'capability'              => 'manage_options', // Cap required to view options-page.
-		// 'position'                => 1, // Menu position. Only applicable if 'parent_slug' is left empty.
-		// 'admin_menu_hook'         => 'network_admin_menu', // 'network_admin_menu' to add network-level options page.
-		// 'priority'                => 10, // Define the page-registration admin menu hook priority.
-		// 'display_cb'              => false, // Override the options-page form output (CMB2_Hookup::options_page_output()).
-		// 'save_button'             => esc_html__( 'Save Theme Options', 'cmb2' ), // The text for the options-page save button. Defaults to 'Save'.
-		// 'disable_settings_errors' => true, // On settings pages (not options-general.php sub-pages), allows disabling.
-		// 'message_cb'              => 'pscm_options_page_message_callback',
-		// 'tab_group'               => '', // Tab-group identifier, enables options page tab navigation.
-		// 'tab_title'               => null, // Falls back to 'title' (above).
-		// 'autoload'                => false, // Defaults to true, the options-page option will be autloaded.
-	) );
-
-	/**
-	 * Options fields ids only need
-	 * to be unique within this box.
-	 * Prefix is not needed.
-	 */
-	$cmb_options->add_field( array(
-		'name'    => esc_html__( 'Site Background Color', 'cmb2' ),
-		'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'      => 'bg_color',
-		'type'    => 'colorpicker',
-		'default' => '#ffffff',
-	) );
-
-}
-
-/**
- * Callback to define the optionss-saved message.
- *
- * @param CMB2  $cmb The CMB2 object.
- * @param array $args {
- *     An array of message arguments
- *
- *     @type bool   $is_options_page Whether current page is this options page.
- *     @type bool   $should_notify   Whether options were saved and we should be notified.
- *     @type bool   $is_updated      Whether options were updated with save (or stayed the same).
- *     @type string $setting         For add_settings_error(), Slug title of the setting to which
- *                                   this error applies.
- *     @type string $code            For add_settings_error(), Slug-name to identify the error.
- *                                   Used as part of 'id' attribute in HTML output.
- *     @type string $message         For add_settings_error(), The formatted message text to display
- *                                   to the user (will be shown inside styled `<div>` and `<p>` tags).
- *                                   Will be 'Settings updated.' if $is_updated is true, else 'Nothing to update.'
- *     @type string $type            For add_settings_error(), Message type, controls HTML class.
- *                                   Accepts 'error', 'updated', '', 'notice-warning', etc.
- *                                   Will be 'updated' if $is_updated is true, else 'notice-warning'.
- * }
- */
-function pscm_options_page_message_callback( $cmb, $args ) {
-	if ( ! empty( $args['should_notify'] ) ) {
-
-		if ( $args['is_updated'] ) {
-
-			// Modify the updated message.
-			$args['message'] = sprintf( esc_html__( '%s &mdash; Updated!', 'cmb2' ), $cmb->prop( 'title' ) );
-		}
-
-		add_settings_error( $args['setting'], $args['code'], $args['message'], $args['type'] );
-	}
-}
-
-/**
- * Only show this box in the CMB2 REST API if the user is logged in.
- *
- * @param  bool                 $is_allowed     Whether this box and its fields are allowed to be viewed.
- * @param  CMB2_REST_Controller $cmb_controller The controller object.
- *                                              CMB2 object available via `$cmb_controller->rest_box->cmb`.
- *
- * @return bool                 Whether this box and its fields are allowed to be viewed.
- */
-function pscm_limit_rest_view_to_logged_in_users( $is_allowed, $cmb_controller ) {
-	if ( ! is_user_logged_in() ) {
-		$is_allowed = false;
-	}
-
-	return $is_allowed;
-}
-
-add_action( 'cmb2_init', 'pscm_register_rest_api_box' );
-/**
- * Hook in and add a box to be available in the CMB2 REST API. Can only happen on the 'cmb2_init' hook.
- * More info: https://github.com/CMB2/CMB2/wiki/REST-API
- */
-function pscm_register_rest_api_box() {
-	$cmb_rest = new_cmb2_box( array(
-		'id'            => 'pscm_rest_metabox',
-		'title'         => esc_html__( 'REST Test Box', 'cmb2' ),
-		'object_types'  => array( 'page' ), // Post type
-		'show_in_rest' => WP_REST_Server::ALLMETHODS, // WP_REST_Server::READABLE|WP_REST_Server::EDITABLE, // Determines which HTTP methods the box is visible in.
-		// Optional callback to limit box visibility.
-		// See: https://github.com/CMB2/CMB2/wiki/REST-API#permissions
-		// 'get_box_permissions_check_cb' => 'pscm_limit_rest_view_to_logged_in_users',
-	) );
-
-	$cmb_rest->add_field( array(
-		'name'       => esc_html__( 'REST Test Text', 'cmb2' ),
-		'desc'       => esc_html__( 'Will show in the REST API for this box and for pages.', 'cmb2' ),
-		'id'         => 'pscm_rest_text',
-		'type'       => 'text',
-	) );
-
-	$cmb_rest->add_field( array(
-		'name'       => esc_html__( 'REST Editable Test Text', 'cmb2' ),
-		'desc'       => esc_html__( 'Will show in REST API "editable" contexts only (`POST` requests).', 'cmb2' ),
-		'id'         => 'pscm_rest_editable_text',
-		'type'       => 'text',
-		'show_in_rest' => WP_REST_Server::EDITABLE,// WP_REST_Server::ALLMETHODS|WP_REST_Server::READABLE, // Determines which HTTP methods the field is visible in. Will override the cmb2_box 'show_in_rest' param.
-	) );
 }
